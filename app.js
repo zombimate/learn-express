be17ffbd621726fcc9ef9e9874cb179ebdfab181
zombimate <gkswk103@naver.com>
2022-12-29 15:57:43 +0900

    추가

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 /* learn-express/app.js */
 const express = require("express");
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const path = require('path');
 
+dotenv.config();
 const app = express();
-const post = process.env.PORT || 8081;
+const port = process.env.PORT || 8888;
 
-app.get("/", (req, res) => {
-  res.sendFile('Hello, Express');
+app.use(morgan('dev'));
+메ㅔㅕㄴㄷ('/', express.static(path.join(__dirname, 'public')));
+
+/* 전처리 미들웨어 */
+app.use((req, res, next) => {
+  console.log('모든 요청에 다 실행됩니다.')
+  next();
+});
+/* 라우트 미들웨어 */
+app.get("/", (req, res, next) => {
+  res.sendFile('GET / 요청에서만 실행됩니다');
+  next();
+}, (req, res) => {
+  throw new Error('에러는 에러처리 미들웨어로 갑니다.')
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.messsage);
 });
 
 app.listen(port, () => {
